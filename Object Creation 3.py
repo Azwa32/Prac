fd3c61558a2944536ac9aaad2e3e1483c7ec35b9
Azwa32 <87596275+Azwa32@users.noreply.github.com>
2023-04-11 22:03:40 +0800

    Update Object Creation 3.py

--- Object Creation 3.py
+++ Object Creation 3.py
@@ -1,5 +1,28 @@
 #Includes inheritance with parent/child classes
 
+"""
+Create a Person class with the following attributes:
+
+name
+age
+gender
+height
+weight
+Create a Student subclass of Person with the additional attributes:
+
+student_id
+major
+gpa
+Create a Teacher subclass of Person with the additional attributes:
+
+teacher_id
+subject
+salary
+For each class, create an __init__ method that initializes the attributes, as well as a display method that prints out all the attributes for the object.
+
+Create at least one instance of each class and call their display method to make sure everything is working correctly.
+"""
+
 class Person:
     def __init__(self, name, age, gender, height, weight):
         self.name = name
