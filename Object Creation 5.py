be6bbef346ecf0e143e63dffa96e066e086bbcdf
Azwa32 <87596275+Azwa32@users.noreply.github.com>
2023-04-11 22:06:27 +0800

    Update Object Creation 5.py

--- Object Creation 5.py
+++ Object Creation 5.py
@@ -1,3 +1,23 @@
+"""
+Write a program that models a car rental system. The program should have two classes: Car and Rental.
+
+The Car class should have the following attributes:
+make: a string representing the make of the car (e.g. "Toyota", "Honda", etc.)
+model: a string representing the model of the car (e.g. "Corolla", "Civic", etc.)
+year: an integer representing the year the car was made (e.g. 2015, 2016, etc.)
+daily_rate: a float representing the daily rental rate for the car
+The Rental class should have the following attributes:
+car: a Car object representing the rented car
+start_date: a string representing the start date of the rental (e.g. "2023-04-03")
+end_date: a string representing the end date of the rental (e.g. "2023-04-05")
+customer_name: a string representing the name of the customer who rented the car
+The Rental class should also have the following methods:
+
+get_rental_period(): a method that returns the number of days between the start date and end date of the rental
+get_rental_cost(): a method that returns the total cost of the rental, which is calculated by multiplying the rental period by the car's daily rate
+Write a program that creates at least two Car objects and at least one Rental object, and displays the rental period and cost for the Rental object.
+"""
+
 from datetime import datetime
 
 #define car class
