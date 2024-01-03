a16d979fa44f43c1947339f411feef11fdf7f91e
AA Mitchell <87596275+Azwa32@users.noreply.github.com>
2024-01-03 02:18:59 +0000

    modified:   Web Automation/main.py

--- Web Automation/main.py
+++ Web Automation/main.py
@@ -7,15 +7,25 @@
 # install any other dependacies for chrome >>sudo apt-get install -f
 # re-install chrome >>sudo dpkg -i google-chrome-stable_current_amd64.deb
 # verify installation >>google-chrome --version
-# download chrome driver >>wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
-# open zip >>unzip chromedriver_linux64.zip
-# move driver into directory included in path >>sudo mv chromedriver /usr/local/bin/
-# remove zip >>rm chromedriver_linux64.zip
-
+# install chrome driver >>pip install selenium chromedriver-binary
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-service = Service(executable_path="")
-driver = webdriver.Chrome(service=service)
+class PriceScraper:
+
+    def scrape(self):
+
+        input_url = "https://www.google.com"
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(input_url)
+
+if __name__ == '__main__':
+    scraper = PriceScraper()
+    scraper.scrape()
 
